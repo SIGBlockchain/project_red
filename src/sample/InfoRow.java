a6c333a2c9e84c84664d50c0cf10647e999e290d
ramirez915 <jose.angelramirez915@gmail.com>
2019-10-08 18:00:28 -0500

    setup code

---
+++ src/sample/InfoRow.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.scene.layout.GridPane;
+
+// 1st row of GUI pertains to the drawn out vision of GUI.
+public class InfoRow {
+
+    // sets up row 1 of the GUI will modify the GridPane
+    // everything that is going to be set into the grid, add it to mainGrid
+    public static void setupInfoRow(GridPane mainGrid){
+        // Labels for text fields
+
+
+        // text fields
+
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,43 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public GridPane GuiGrid = new GridPane();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Aurum Project Red");
+        primaryStage.setScene(MainWindowSetup());
+        primaryStage.show();
+    }
+
+    Scene MainWindowSetup(){
+        // add info row to main GridPane
+        InfoRow.setupInfoRow(GuiGrid);
+
+        // add Send Aurum fields
+        SendAurumRow.setupSendAurumRow(GuiGrid);
+
+        // add last row
+        MiscRow.setupMiscRow(GuiGrid);
+
+        // return new Scene using GridPane GuiGrid with all of its additions
+        return new Scene(GuiGrid,1000,800);
+    }
+
+
+}
