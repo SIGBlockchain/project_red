b10a2dbcb766d3285b894d5f34de0b88665a1a4d
ramirez915 <jose.angelramirez915@gmail.com>
2019-10-08 20:41:08 -0500

    updates to set up code

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -26,6 +27,19 @@ public class Main extends Application {
     }
 
     Scene MainWindowSetup(){
+        GuiGrid.setPadding(new Insets(10, 10, 10, 10));
+        GuiGrid.setVgap(80);        // Vgap and Hgap are subject to change
+        GuiGrid.setHgap(80);
+
+        //************************************** uncomment to see example of grid after commenting out the following 3 functions
+
+//        for(int i = 0; i < 6; i++){
+//            for(int j = 0; j < 4; j++){
+//                Label l1 = new Label("test" + i);
+//                GuiGrid.add(l1,i,j);
+//            }
+//        }
+
         // add info row to main GridPane
         InfoRow.setupInfoRow(GuiGrid);
 
@@ -36,8 +50,6 @@ public class Main extends Application {
         MiscRow.setupMiscRow(GuiGrid);
 
         // return new Scene using GridPane GuiGrid with all of its additions
-        return new Scene(GuiGrid,1000,800);
+        return new Scene(GuiGrid,800,800);                                  // size of the scene is subject to change as well
     }
-
-
 }
