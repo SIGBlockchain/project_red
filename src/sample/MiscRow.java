22cf8404abe593cded0e4c27226ab3691c25b986
HarryL5004 <loklok8795@gmail.com>
2019-10-20 22:12:32 -0500

    Added Update Button

--- src/sample/MiscRow.java
+++ src/sample/MiscRow.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 // 3rd row that will be used for messages from producer and future additions
@@ -10,6 +11,10 @@ public class MiscRow {
 
 
         // text fields
+
+        // update button
+        Button updateBtn = new Button("Update");
+        grid.add(updateBtn, 1,2);
 
     }
 }
