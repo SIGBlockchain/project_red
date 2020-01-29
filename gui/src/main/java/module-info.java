5975cad42264bdd3753d5f47f8c49742e24fb94c
Vineet <vineetrpatel@gmail.com>
2020-01-28 23:51:32 -0600

    Minor changes

--- gui/src/main/java/module-info.java
+++ gui/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module sigblockchain.projectred.gui {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens sigblockchain.projectred.gui to javafx.fxml;
+    opens sigblockchain.projectred.gui to javafx.fxml, javafx.graphics;
     exports sigblockchain.projectred.gui;
-
 }
