55a50a4cb8e528936725948323f9676b41c633a6
Vineet Patel <vineetrpatel@gmail.com>
2020-02-03 23:13:15 -0600

    Able to generate private and public key, along with corresponding unit tests.

--- keys/src/main/java/sigblockchain/projectred/keys/KeyManager.java
+++ keys/src/main/java/sigblockchain/projectred/keys/KeyManager.java
@@ -1,4 +1,29 @@
 package sigblockchain.projectred.keys;
 
+import org.bouncycastle.asn1.sec.SECNamedCurves;
+import org.bouncycastle.asn1.x9.X9ECParameters;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
+import org.bouncycastle.crypto.params.ECDomainParameters;
+import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
+
+import java.security.SecureRandom;
+
 public class KeyManager {
+
+    public static final X9ECParameters ecp = SECNamedCurves.getByName("secp256r1");
+    public static final ECDomainParameters domainParams = new ECDomainParameters(ecp.getCurve(),
+            ecp.getG(), ecp.getN(), ecp.getH(), ecp.getSeed());
+
+    /**
+     * Will generate a public/private key pair according to the NIST P-256 standards.
+     *
+     * @return a AsymmetricCipherKeyPair object
+     */
+    public static AsymmetricCipherKeyPair generateKeyPair() {
+        ECKeyGenerationParameters keyGenParams = new ECKeyGenerationParameters(domainParams, new SecureRandom());
+        ECKeyPairGenerator generator = new ECKeyPairGenerator();
+        generator.init(keyGenParams);
+        return generator.generateKeyPair();
+    }
 }
--- keys/src/test/java/sigblockchain/projectred/keys/KeyManagerTest.java
+++ keys/src/test/java/sigblockchain/projectred/keys/KeyManagerTest.java
@@ -1,115 +1,57 @@
 package sigblockchain.projectred.keys;
 
+import org.bouncycastle.asn1.sec.SECNamedCurves;
+import org.bouncycastle.asn1.x9.X9ECParameters;
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
+import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
+import org.bouncycastle.crypto.params.ECDomainParameters;
+import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
+import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
+import org.bouncycastle.crypto.params.ECPublicKeyParameters;
+import org.bouncycastle.math.ec.ECPoint;
 import org.bouncycastle.math.ec.FixedPointCombMultiplier;
-import org.bouncycastle.math.ec.WNafUtil;
-import org.bouncycastle.util.BigIntegers;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.Arrays;
 
-import org.bouncycastle.asn1.x9.X9ECParameters;
-import org.bouncycastle.asn1.sec.SECNamedCurves;
-import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
-import org.bouncycastle.crypto.params.ECDomainParameters;
-import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
-import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
-import org.bouncycastle.crypto.params.ECPublicKeyParameters;
-import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
-import org.bouncycastle.math.ec.ECPoint;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class KeyManagerTest {
+    
+    @Test
+    public void testGenerateKeyPair() {
+        var keyPair = KeyManager.generateKeyPair();
+        var privateKey = (ECPrivateKeyParameters) keyPair.getPrivate();
+        var actualPublicKey = (ECPublicKeyParameters) keyPair.getPublic();
 
-//    @Test
-//    void testGeneratePrivateKey() {
-//        ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256k1");
-//        KeyPairGenerator g = null;
-//        try {
-//            g = KeyPairGenerator.getInstance("EC");
-//        } catch (NoSuchAlgorithmException e) {
-//            e.printStackTrace();
-//        }
-//        try {
-//            g.initialize(ecSpec, new SecureRandom());
-//        } catch (InvalidAlgorithmParameterException e) {
-//            e.printStackTrace();
-//        }
-//        KeyPair keypair = g.generateKeyPair();
-//        PublicKey publicKey = keypair.getPublic();
-//        PrivateKey privateKey = keypair.getPrivate();
-//
-//    }
+        ECPoint Q = KeyManager.domainParams.getG().multiply(privateKey.getD());
+        var expectedPublicKey = new ECPublicKeyParameters(Q, KeyManager.domainParams);
+        assertEquals(actualPublicKey.getQ().getRawXCoord().toBigInteger(), expectedPublicKey.getQ().getRawXCoord().toBigInteger());
+        assertEquals(actualPublicKey.getQ().getRawYCoord().toBigInteger(), expectedPublicKey.getQ().getRawYCoord().toBigInteger());
 
-    public static String toHex(byte[] data) {
-        StringBuilder sb = new StringBuilder();
-        for (byte b : data) {
-            sb.append(String.format("%02x", b & 0xff));
-        }
-        return sb.toString();
     }
 
-    @Test
-    void testGeneratePrivateKey2() {
-        // Get domain parameters for example curve secp256r1
-        X9ECParameters ecp = SECNamedCurves.getByName("secp256r1");
-        ECDomainParameters domainParams = new ECDomainParameters(ecp.getCurve(),
-                ecp.getG(), ecp.getN(), ecp.getH(), ecp.getSeed());
-
-        // Generate a private key and a public key
-        AsymmetricCipherKeyPair keyPair;
-        ECKeyGenerationParameters keyGenParams = new ECKeyGenerationParameters(domainParams, new SecureRandom());
-        ECKeyPairGenerator generator = new ECKeyPairGenerator();
-        generator.init(keyGenParams);
-        keyPair = generator.generateKeyPair();
-
-        ECPrivateKeyParameters privateKey = (ECPrivateKeyParameters) keyPair.getPrivate();
-        ECPublicKeyParameters publicKey = (ECPublicKeyParameters) keyPair.getPublic();
-        byte[] privateKeyBytes = privateKey.getD().toByteArray();
-
-
-        // First print our generated private key and public key
-        System.out.println("Private key: " + toHex(privateKeyBytes));
-        System.out.println("Public key (Compressed): " + toHex(publicKey.getQ().getEncoded(true)));
-
-        // Then calculate the public key only using domainParams.getG() and private key
-        ECPoint Q = domainParams.getG().multiply(new BigInteger(privateKeyBytes));
-        System.out.println("Calculated public key: " + toHex(Q.getEncoded(true)));
-
-//        // The calculated public key and generated public key should always match
-//        if (!toHex(publicKey.getQ().getEncoded(true)).equals(toHex(Q.getEncoded(true)))) {
-//            System.out.println("ERROR: Public keys do not match!");
-//        } else {
-//            System.out.println("Congratulations, public keys match!");
-//        }
-    }
 
     @Test
-    void testGeneratePrivateKey3() {
-        // private key: 0x564632e0c0b640973222cd2130a0142f8d681e86f832ccecb3ef7128c553b9c1
-        // public key: 0xb4d04214fc2be35a4740f15521ea7dcefb4a802a16200fb6bcfd2050be49d90adec09ca4df30e8471bfff3a044a96400f46adf0ca6c0118c98d9a85331bfadf8
-
-        X9ECParameters ecp = SECNamedCurves.getByName("secp256r1");
-        var domainParams = new ECDomainParameters(ecp.getCurve(),
-                ecp.getG(), ecp.getN(), ecp.getH(), ecp.getSeed());
-
-        System.out.printf("secp256r1 parameters: \nG: %s \nN: 0x%s \nH: %s \nSeed: %s\n", ecp.getG().toString(), ecp.getN().toString(16),
-                ecp.getH().toString(16), Arrays.toString(ecp.getSeed()));
-
-
-        var n = new BigInteger("564632e0c0b640973222cd2130a0142f8d681e86f832ccecb3ef7128c553b9c1", 16);
-        System.out.printf("Imported private key: \n%s\n", n.toString(16));
-
+    public void testEcParameters() {
+        // an example of the P-256 public private key pair.
+        var expectedPrivateKey = new BigInteger("6548d29d5533079e28d5c0edecb6394ed4c0342bfa475fdc96c7bf978492f23d", 16);
+        var expectedPublicKeyX = new BigInteger("fac843c2ccf0246eb00cb90e62dd96c64b669e2b867c0c2aaa835801aeae8295", 16);
+        var expectedPublicKeyY = new BigInteger("a797eb07a5d8eef80d5705a7a627c73cf4e683ab98c7245101025be935c4053b", 16);
 
         var fpMultiplier = new FixedPointCombMultiplier();
-        ECPoint Q = fpMultiplier.multiply(domainParams.getG(), n);
-//
-        var publicKey = new ECPublicKeyParameters(Q, domainParams);
-        var privateKey = new ECPrivateKeyParameters(n, domainParams);
+        ECPoint Q = fpMultiplier.multiply(KeyManager.domainParams.getG(), expectedPrivateKey);
 
-        System.out.printf("Private key: %s\n", privateKey.getD().toString(16));
-        System.out.printf("Public key: %s%s\n", publicKey.getQ().getRawXCoord().toBigInteger().toString(16), publicKey.getQ().getRawYCoord().toBigInteger().toString(16));
+        var actualPublicKey = new ECPublicKeyParameters(Q, KeyManager.domainParams);
+        var actualPrivateKey = new ECPrivateKeyParameters(expectedPrivateKey, KeyManager.domainParams);
+
+        assertEquals(actualPrivateKey.getD(), expectedPrivateKey);
+        assertEquals(actualPublicKey.getQ().getRawXCoord().toBigInteger(), expectedPublicKeyX);
+        assertEquals(actualPublicKey.getQ().getRawYCoord().toBigInteger(), expectedPublicKeyY);
 
     }
 }
