22584d9bfbc86b942a3444557db74039c8838ff5
Akshantjain <akshant.jain2@gmail.com>
2020-02-06 19:01:40 -0600

    updated block class and block tests

--- client/src/main/java/sigblockchain/projectred/client/Block.java
+++ client/src/main/java/sigblockchain/projectred/client/Block.java
@@ -1,4 +1,49 @@
 package sigblockchain.projectred.client;
 
 public class Block {
+    private Integer Version;
+    private Integer Height;
+    private Integer Timestamp;
+    private String PreviousHash;
+    private String MerkleRootHash;
+    private Integer DataLen;
+    private String Data;
+
+    public Block(Integer version, Integer height, Integer timestamp, String previousHash, String merkleRootHash, Integer dataLen, String data) {
+        Version = version;
+        Height = height;
+        Timestamp = timestamp;
+        PreviousHash = previousHash;
+        MerkleRootHash = merkleRootHash;
+        DataLen = dataLen;
+        Data = data;
+    }
+
+    public Integer getVersion() {
+        return Version;
+    }
+
+    public Integer getHeight() {
+        return Height;
+    }
+
+    public Integer getTimestamp() {
+        return Timestamp;
+    }
+
+    public String getPreviousHash() {
+        return PreviousHash;
+    }
+
+    public String getMerkleRootHash() {
+        return MerkleRootHash;
+    }
+
+    public Integer getDataLen() {
+        return DataLen;
+    }
+
+    public String getData() {
+        return Data;
+    }
 }
--- client/src/test/java/sigblockchain/projectred/client/BlockTest.java
+++ client/src/test/java/sigblockchain/projectred/client/BlockTest.java
@@ -1,4 +1,63 @@
 package sigblockchain.projectred.client;
 
-public class BlockTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlockTest {
+
+    @Test
+    void testConstructor(){
+        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
+        assertNotNull(block);
+    }
+
+    @Test
+    void versionGetter()    {
+        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
+        int v = block.getVersion();
+        assertEquals(v, 10);
+    }
+
+    @Test
+    void heightGetter()    {
+        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
+        int v = block.getHeight();
+        assertEquals(v, 20);
+    }
+
+    @Test
+    void timeStampGetter()    {
+        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
+        int v = block.getTimestamp();
+        assertEquals(v, 30);
+    }
+
+    @Test
+    void previousHashGetter()    {
+        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
+        String v = block.getPreviousHash();
+        assertEquals(v, "hello");
+    }
+
+    @Test
+    void merkleRootHashGetter()    {
+        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
+        String v = block.getMerkleRootHash();
+        assertEquals(v, "blockChain");
+    }
+
+    @Test
+    void dataLenGetter()    {
+        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
+        int v = block.getDataLen();
+        assertEquals(v, 10);
+    }
+
+    @Test
+    void dataGetter()    {
+        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
+        String v = block.getData();
+        assertEquals(v, "Data");
+    }
 }
