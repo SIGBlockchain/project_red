6ca19d646bbcddc67e32fc2ae1492f42c30cdb21
Akshantjain <akshant.jain2@gmail.com>
2020-02-08 21:55:00 -0600

    Block Class and Test updated

--- client/src/main/java/sigblockchain/projectred/client/Block.java
+++ client/src/main/java/sigblockchain/projectred/client/Block.java
@@ -1,15 +1,28 @@
 package sigblockchain.projectred.client;
 
 public class Block {
-    private Integer Version;
-    private Integer Height;
-    private Integer Timestamp;
-    private String PreviousHash;
-    private String MerkleRootHash;
-    private Integer DataLen;
-    private String Data;
+    private final int Version;          // uint16
+    private final long Height;          // uint64
+    private final long Timestamp;       // int64
+    private final byte[] PreviousHash;  // []byte
+    private final byte[] MerkleRootHash;// []byte
+    private final int DataLen;          // uint16
+    private final byte[][] Data;        // [][]byte
 
-    public Block(Integer version, Integer height, Integer timestamp, String previousHash, String merkleRootHash, Integer dataLen, String data) {
+    public Block(int version, long height, long timestamp, byte[] previousHash, byte[] merkleRootHash, int dataLen, byte[][] data) {
+        if (!checkCompatibility(version, height, timestamp, dataLen))
+            throw new IllegalArgumentException("Invalid Input Values");
+
+//        //Individual values Checking
+//        if (!checkUnsigned16(version))
+//            throw new IllegalArgumentException("Invalid Input version");
+//        if (!checkUnsigned64(height))
+//            throw new IllegalArgumentException("Invalid Input height");
+//        if (!checkSigned64(timestamp))
+//            throw new IllegalArgumentException("Invalid Input timeStamp");
+//        if (!checkUnsigned16(dataLen))
+//            throw new IllegalArgumentException("Invalid Input dataLen");
+
         Version = version;
         Height = height;
         Timestamp = timestamp;
@@ -19,31 +32,51 @@ public class Block {
         Data = data;
     }
 
-    public Integer getVersion() {
+    private boolean checkCompatibility(int version,
+                                       long height,
+                                       long timestamp,
+                                       int dataLen)   {
+
+        return checkUnsigned16(version) && checkUnsigned64(height) && checkSigned64(timestamp) && checkUnsigned16(dataLen);
+    }
+
+    private boolean checkUnsigned16(int value)   {
+        return (value >= 0) && (value < (65536));
+    }
+
+    private boolean checkUnsigned64(long value)   {
+        return value >=0;
+    }
+
+    private boolean checkSigned64(long value) {
+        return (value >= -(Math.pow(2, 63))) && value < (Math.pow(2, 63));
+    }
+
+    public int getVersion() {
         return Version;
     }
 
-    public Integer getHeight() {
+    public long getHeight() {
         return Height;
     }
 
-    public Integer getTimestamp() {
+    public long getTimestamp() {
         return Timestamp;
     }
 
-    public String getPreviousHash() {
+    public byte[] getPreviousHash() {
         return PreviousHash;
     }
 
-    public String getMerkleRootHash() {
+    public byte[] getMerkleRootHash() {
         return MerkleRootHash;
     }
 
-    public Integer getDataLen() {
+    public int getDataLen() {
         return DataLen;
     }
 
-    public String getData() {
+    public byte[][] getData() {
         return Data;
     }
 }
--- client/src/test/java/sigblockchain/projectred/client/BlockTest.java
+++ client/src/test/java/sigblockchain/projectred/client/BlockTest.java
@@ -6,58 +6,68 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BlockTest {
 
+    byte[] previousHash = new byte[] {'T', 'I', 'M', 'E'};
+    byte[] hash = new byte[] {1, 2, 3, 4};
+    byte[][] data =
+            {
+                    {1, 2, 3, 4},
+                    {'H', 'E', 'L', 'L', 'O'},
+                    {5, 6, 7, 8},
+                    {'W', 'O', 'R', 'L', 'D'}
+            };
+
     @Test
     void testConstructor(){
-        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
+        Block block = new Block(10, 401L, -3556L, previousHash, hash,10, data);
         assertNotNull(block);
     }
 
     @Test
     void versionGetter()    {
-        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
+        Block block = new Block(10, 401L, -3556L, previousHash, hash,10, data);
         int v = block.getVersion();
         assertEquals(v, 10);
     }
 
     @Test
     void heightGetter()    {
-        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
-        int v = block.getHeight();
-        assertEquals(v, 20);
+        Block block = new Block(10, 401L, -3556L, previousHash, hash,10, data);
+        long v = block.getHeight();
+        assertEquals(v, 401L);
     }
 
     @Test
     void timeStampGetter()    {
-        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
-        int v = block.getTimestamp();
-        assertEquals(v, 30);
+        Block block = new Block(10, 401L, -3556L, previousHash, hash,10, data);
+        long v = block.getTimestamp();
+        assertEquals(v, -3556L);
     }
 
     @Test
     void previousHashGetter()    {
-        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
-        String v = block.getPreviousHash();
-        assertEquals(v, "hello");
+        Block block = new Block(10, 401L, -3556L, previousHash, hash,10, data);
+        byte[] v = block.getPreviousHash();
+        assertEquals(v, previousHash);
     }
 
     @Test
     void merkleRootHashGetter()    {
-        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
-        String v = block.getMerkleRootHash();
-        assertEquals(v, "blockChain");
+        Block block = new Block(10, 401L, -3556L, previousHash, hash,10, data);
+        byte[] v = block.getMerkleRootHash();
+        assertEquals(v, hash);
     }
 
     @Test
     void dataLenGetter()    {
-        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
+        Block block = new Block(10, 401L, -3556L, previousHash, hash,10, data);
         int v = block.getDataLen();
         assertEquals(v, 10);
     }
 
     @Test
     void dataGetter()    {
-        Block block = new Block(10, 20, 30, "hello", "blockChain", 10, "Data");
-        String v = block.getData();
-        assertEquals(v, "Data");
+        Block block = new Block(10, 401L, -3556L, previousHash, hash,10, data);
+        byte[][] v = block.getData();
+        assertEquals(v, data);
     }
 }
