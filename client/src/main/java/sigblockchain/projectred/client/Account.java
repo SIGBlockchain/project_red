5109299547f84b4b94b77c3595a432d42124ce99
dhodcz2 <dhodcz2@uic.edu>
2020-02-12 11:22:13 -0600

    Here are my changes. I have left the Wallet validation commented as a placeholder for Aurum specifications.

--- client/src/main/java/sigblockchain/projectred/client/Account.java
+++ client/src/main/java/sigblockchain/projectred/client/Account.java
@@ -1,64 +1,77 @@
 package sigblockchain.projectred.client;
-
 import java.math.BigInteger;
 
 public class Account {
-	private String WalletAddress;
-	private BigInteger Balance;
-	private BigInteger StateNonce;
+    private String walletAddress;
+    private BigInteger balance;
+    private BigInteger stateNonce;
 
-	public Account(String walletAddress, BigInteger balance, BigInteger stateNonce) {
-		validateWalletAddress(walletAddress);
+    public Account(String walletAddress, BigInteger balance, BigInteger stateNonce) {
+//        validateWalletAddress(walletAddress);
+        validatePositive(balance, "Balance");
+        validatePositive(stateNonce, "stateNonce");
 
-		WalletAddress = walletAddress;
-		Balance = balance;
-		StateNonce = stateNonce;
-	}
+        this.walletAddress = walletAddress;
+        this.balance = balance;
+        this.stateNonce = stateNonce;
+    }
 
-	public Account(String walletAddress) {
-		validateWalletAddress(walletAddress);
+    public Account(String walletAddress)
+    {
+//        validateWalletAddress(walletAddress);
 
-		WalletAddress = walletAddress;
-		Balance = BigInteger.ZERO;
-		StateNonce = BigInteger.ZERO;
-	}
+        this.walletAddress = walletAddress;
+        this.balance = BigInteger.ZERO;
+        this.stateNonce = BigInteger.ZERO;
+    }
 
-	private void validateWalletAddress(String walletAddress) {
-        //        WalletAddress must not contain 'I', 'l', '0', or 'O'.d
-		for (String invalidChar : new String[] {"I", "l", "0", "O"}) {
-			if (walletAddress.contains(invalidChar)) {
-				throw new IllegalArgumentException("Invalid WalletAddress: Illegal character(s).");
-			}
-		}
-        //        WalletAddress must contain between 25 and 34 characters.
-		if ((walletAddress.length() < 25) || (walletAddress.length() > 34)) {
-			throw new IllegalArgumentException("Invalid WalletAddress: Illegal length.");
-		}
-        //        WalletAddress must begin with 1, 3, or bc1.
-		if (!(walletAddress.startsWith("1") || walletAddress.startsWith("3") || walletAddress.startsWith("bc1"))) {
-			throw new IllegalArgumentException("Invalid WalletAddress: Illegal header.");
-		}
-	}
+    private void validateWalletAddress (String walletAddress){
+//        Todo: Validation for Aurum Wallet Input Address; these are for Bitcoin and adjustments will be made.
 
-	public String getWalletAddress() {
-		return WalletAddress;
-	}
+//        walletAddress must not contain 'I', 'l', '0', or 'O'.d
+        for(String invalidChar : new String[]{"I", "l", "0", "O"})
+        {
+            if (walletAddress.contains(invalidChar)) {
+                throw new IllegalArgumentException("Invalid walletAddress: Illegal character(s).");
+            }
+        }
+//        walletAddress must contain between 25 and 34 characters.
+        if ((walletAddress.length() < 25) || (walletAddress.length() > 34)) {
+            throw new IllegalArgumentException("Invalid walletAddress: Illegal length.");
+        }
+//        walletAddress must begin with 1, 3, or bc1.
+        if (!(walletAddress.startsWith("1") || walletAddress.startsWith("3") || walletAddress.startsWith("bc1")))
+        {
+            throw new IllegalArgumentException("Invalid walletAddress: Illegal header.");
+        }
+    }
 
-	public BigInteger getBalance() {
-		return Balance;
-	}
+    private void validatePositive(BigInteger n, String s) {
+//        If 0 > n; raise exception
+        if (BigInteger.ZERO.compareTo(n) > 0) {
+            throw new IllegalArgumentException(String.format("Invalid %s: Negative integer.", s));
+        }
+    }
 
-	public BigInteger getStateNonce() {
-		return StateNonce;
-	}
+    public String getWalletAddress() {
+        return this.walletAddress;
+    }
 
-	public void setBalance(BigInteger balance) {
-		Balance = balance;
-	}
+    public BigInteger getBalance() { return this.balance; }
 
-	public void setStateNonce(BigInteger stateNonce) {
-		StateNonce = stateNonce;
-	}
+    public BigInteger getStateNonce() {
+        return this.stateNonce;
+    }
+
+    public void setBalance(BigInteger balance) {
+        validatePositive(balance, "balance");
+        this.balance = balance;
+    }
+
+    public void setStateNonce(BigInteger stateNonce) {
+        validatePositive(stateNonce, "stateNonce");
+        this.stateNonce = stateNonce;
+    }
 }
 
 
