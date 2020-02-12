0fa75c4addbfad17e7c269d31c0856c118ebcce2
Vineet Patel <vineetrpatel@gmail.com>
2020-02-12 01:35:09 -0600

    Formatted and added unit tests.

--- client/src/main/java/sigblockchain/projectred/client/Account.java
+++ client/src/main/java/sigblockchain/projectred/client/Account.java
@@ -1,65 +1,64 @@
 package sigblockchain.projectred.client;
 
+import java.math.BigInteger;
+
 public class Account {
-    private String WalletAddress;
-    private long Balance;
-    private long StateNonce;
+	private String WalletAddress;
+	private BigInteger Balance;
+	private BigInteger StateNonce;
 
-    public Account(String walletAddress, long balance, long stateNonce) {
-        validateWalletAddress(walletAddress);
+	public Account(String walletAddress, BigInteger balance, BigInteger stateNonce) {
+		validateWalletAddress(walletAddress);
 
-        WalletAddress = walletAddress;
-        Balance = balance;
-        StateNonce = stateNonce;
-    }
+		WalletAddress = walletAddress;
+		Balance = balance;
+		StateNonce = stateNonce;
+	}
 
-    public Account(String walletAddress)
-    {
-        validateWalletAddress(walletAddress);
+	public Account(String walletAddress) {
+		validateWalletAddress(walletAddress);
 
-        WalletAddress = walletAddress;
-        Balance = 0;
-        StateNonce = 0;
-    }
+		WalletAddress = walletAddress;
+		Balance = BigInteger.ZERO;
+		StateNonce = BigInteger.ZERO;
+	}
 
-    private void validateWalletAddress (String walletAddress){
-//        WalletAddress must not contain 'I', 'l', '0', or 'O'.d
-        for(String invalidChar : new String[]{"I", "l", "0", "O"})
-        {
-            if (walletAddress.contains(invalidChar)) {
-                throw new IllegalArgumentException("Invalid WalletAddress: Illegal character(s).");
-            }
-        }
-//        WalletAddress must contain between 25 and 34 characters.
-        if ((walletAddress.length() < 25) || (walletAddress.length() > 34)) {
-            throw new IllegalArgumentException("Invalid WalletAddress: Illegal length.");
-        }
-//        WalletAddress must begin with 1, 3, or bc1.
-        if (!(walletAddress.startsWith("1") || walletAddress.startsWith("3") || walletAddress.startsWith("bc1")))
-        {
-            throw new IllegalArgumentException("Invalid WalletAddress: Illegal header.");
-        }
-    }
+	private void validateWalletAddress(String walletAddress) {
+        //        WalletAddress must not contain 'I', 'l', '0', or 'O'.d
+		for (String invalidChar : new String[] {"I", "l", "0", "O"}) {
+			if (walletAddress.contains(invalidChar)) {
+				throw new IllegalArgumentException("Invalid WalletAddress: Illegal character(s).");
+			}
+		}
+        //        WalletAddress must contain between 25 and 34 characters.
+		if ((walletAddress.length() < 25) || (walletAddress.length() > 34)) {
+			throw new IllegalArgumentException("Invalid WalletAddress: Illegal length.");
+		}
+        //        WalletAddress must begin with 1, 3, or bc1.
+		if (!(walletAddress.startsWith("1") || walletAddress.startsWith("3") || walletAddress.startsWith("bc1"))) {
+			throw new IllegalArgumentException("Invalid WalletAddress: Illegal header.");
+		}
+	}
 
-    public String getWalletAddress() {
-        return WalletAddress;
-    }
+	public String getWalletAddress() {
+		return WalletAddress;
+	}
 
-    public long getBalance() {
-        return Balance;
-    }
+	public BigInteger getBalance() {
+		return Balance;
+	}
 
-    public long getStateNonce() {
-        return StateNonce;
-    }
+	public BigInteger getStateNonce() {
+		return StateNonce;
+	}
 
-    public void setBalance(long balance) {
-        Balance = balance;
-    }
+	public void setBalance(BigInteger balance) {
+		Balance = balance;
+	}
 
-    public void setStateNonce(long stateNonce) {
-        StateNonce = stateNonce;
-    }
+	public void setStateNonce(BigInteger stateNonce) {
+		StateNonce = stateNonce;
+	}
 }
 
 
