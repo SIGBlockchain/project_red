62970996ebd6b70ff6ad2548502cf645e4755ef6
Vineet Patel <vineetrpatel@gmail.com>
2020-02-21 01:17:23 -0600

    TestFx is now able to click buttons.

--- gui/src/test/java/sigblockchain/projectred/gui/MultiplePaneTest.java
+++ gui/src/test/java/sigblockchain/projectred/gui/MultiplePaneTest.java
@@ -3,9 +3,12 @@ package sigblockchain.projectred.gui;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
@@ -22,7 +25,10 @@ public class MultiplePaneTest {
 	}
 
 	@Test
-	public void testSwitchingPane() {
+	public void testSwitchingPane(FxRobot robot) {
+		robot.clickOn("#navKeyBtn");
+
+		//Assertions.assertThat(robot.lookup("#myButton").queryAs(Button.class)).hasText("click me!");
 
 	}
 }
