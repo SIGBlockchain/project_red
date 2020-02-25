c23943651f03b016e72cb5cd3bc6b76317454ad1
Vineet77 <vineetrpatel@gmail.com>
2020-02-25 13:15:43 -0600

    Added checkstyle configuration

--- keys/src/main/java/sigblockchain/projectred/keys/KeyManager.java
+++ keys/src/main/java/sigblockchain/projectred/keys/KeyManager.java
@@ -1,5 +1,6 @@
 package sigblockchain.projectred.keys;
 
+import java.security.SecureRandom;
 import org.bouncycastle.asn1.sec.SECNamedCurves;
 import org.bouncycastle.asn1.x9.X9ECParameters;
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
@@ -7,23 +8,21 @@ import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
 import org.bouncycastle.crypto.params.ECDomainParameters;
 import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
 
-import java.security.SecureRandom;
-
 public class KeyManager {
 
-    public static final X9ECParameters ecp = SECNamedCurves.getByName("secp256r1");
-    public static final ECDomainParameters domainParams = new ECDomainParameters(ecp.getCurve(),
-            ecp.getG(), ecp.getN(), ecp.getH(), ecp.getSeed());
+	public static final X9ECParameters ecp = SECNamedCurves.getByName("secp256r1");
+	public static final ECDomainParameters domainParams = new ECDomainParameters(ecp.getCurve(),
+		ecp.getG(), ecp.getN(), ecp.getH(), ecp.getSeed());
 
-    /**
-     * Will generate a public/private key pair according to the NIST P-256 standards.
-     *
-     * @return a AsymmetricCipherKeyPair object
-     */
-    public static AsymmetricCipherKeyPair generateKeyPair() {
-        ECKeyGenerationParameters keyGenParams = new ECKeyGenerationParameters(domainParams, new SecureRandom());
-        ECKeyPairGenerator generator = new ECKeyPairGenerator();
-        generator.init(keyGenParams);
-        return generator.generateKeyPair();
-    }
+	/**
+	 * Will generate a public/private key pair according to the NIST P-256 standards.
+	 *
+	 * @return a AsymmetricCipherKeyPair object
+	 */
+	public static AsymmetricCipherKeyPair generateKeyPair() {
+		var keyGenParams = new ECKeyGenerationParameters(domainParams, new SecureRandom());
+		var generator = new ECKeyPairGenerator();
+		generator.init(keyGenParams);
+		return generator.generateKeyPair();
+	}
 }
