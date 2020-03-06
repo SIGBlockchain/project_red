8fbde24939b06c1245f87eb78a95aab6ae10bb51
Jacob DiStefano <jldstefano@gmail.com>
2020-03-05 19:18:53 -0600

    Fix?

--- keys/src/test/java/sigblockchain/projectred/keys/KeyManagerTest.java
+++ keys/src/test/java/sigblockchain/projectred/keys/KeyManagerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 import java.math.BigInteger;
 import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
 import org.bouncycastle.crypto.params.ECPublicKeyParameters;
@@ -14,36 +15,23 @@ import org.junit.jupiter.api.Test;
 
 class KeyManagerTest {
 
-  @Test
-  public void testGenerateKeyPair() {
-    var keyPair = KeyManager.generateKeyPair();
-    var privateKey = (ECPrivateKeyParameters) keyPair.getPrivate();
-    var actualPublicKey = (ECPublicKeyParameters) keyPair.getPublic();
+	@Test
+	public void testEcParameters() {
+		// an example of the P-256 public private key pair.
+		var expectedPrivateKey = new BigInteger("6548d29d5533079e28d5c0edecb6394ed4c0342bfa475fdc96c7bf978492f23d", 16);
+		var expectedPublicKeyX = new BigInteger("fac843c2ccf0246eb00cb90e62dd96c64b669e2b867c0c2aaa835801aeae8295", 16);
+		var expectedPublicKeyY = new BigInteger("a797eb07a5d8eef80d5705a7a627c73cf4e683ab98c7245101025be935c4053b", 16);
 
-    ECPoint Q = KeyManager.domainParams.getG().multiply(privateKey.getD());
-    var expectedPublicKey = new ECPublicKeyParameters(Q, KeyManager.domainParams);
-    assertEquals(actualPublicKey.getQ().getRawXCoord().toBigInteger(), expectedPublicKey.getQ().getRawXCoord().toBigInteger());
-    assertEquals(actualPublicKey.getQ().getRawYCoord().toBigInteger(), expectedPublicKey.getQ().getRawYCoord().toBigInteger());
+		var fpMultiplier = new FixedPointCombMultiplier();
+		ECPoint Q = fpMultiplier.multiply(KeyManager.domainParams.getG(), expectedPrivateKey);
 
-  }
+		var actualPublicKey = new ECPublicKeyParameters(Q, KeyManager.domainParams);
+		var actualPrivateKey = new ECPrivateKeyParameters(expectedPrivateKey, KeyManager.domainParams);
 
-
-  @Test
-  public void testEcParameters() {
-    // an example of the P-256 public private key pair.
-    var expectedPrivateKey = new BigInteger("6548d29d5533079e28d5c0edecb6394ed4c0342bfa475fdc96c7bf978492f23d", 16);
-    var expectedPublicKeyX = new BigInteger("fac843c2ccf0246eb00cb90e62dd96c64b669e2b867c0c2aaa835801aeae8295", 16);
-    var expectedPublicKeyY = new BigInteger("a797eb07a5d8eef80d5705a7a627c73cf4e683ab98c7245101025be935c4053b", 16);
-
-    var fpMultiplier = new FixedPointCombMultiplier();
-    ECPoint Q = fpMultiplier.multiply(KeyManager.domainParams.getG(), expectedPrivateKey);
-
-    var actualPublicKey = new ECPublicKeyParameters(Q, KeyManager.domainParams);
-    var actualPrivateKey = new ECPrivateKeyParameters(expectedPrivateKey, KeyManager.domainParams);
-
-    assertEquals(actualPrivateKey.getD(), expectedPrivateKey);
-    assertEquals(actualPublicKey.getQ().getRawXCoord().toBigInteger(), expectedPublicKeyX);
-    assertEquals(actualPublicKey.getQ().getRawYCoord().toBigInteger(), expectedPublicKeyY);
+		assertEquals(actualPrivateKey.getD(), expectedPrivateKey);
+		assertEquals(actualPublicKey.getQ().getRawXCoord().toBigInteger(), expectedPublicKeyX);
+		assertEquals(actualPublicKey.getQ().getRawYCoord().toBigInteger(), expectedPublicKeyY);
+	}
 
 	@Test
 	public void testGenerateKeyPair() {
@@ -54,13 +42,13 @@ class KeyManagerTest {
 		ECPoint q = KeyManager.domainParams.getG().multiply(privateKey.getD());
 		var expectedPublicKey = new ECPublicKeyParameters(q, KeyManager.domainParams);
 		assertEquals(actualPublicKey.getQ().getRawXCoord().toBigInteger(),
-				expectedPublicKey.getQ().getRawXCoord().toBigInteger());
+			expectedPublicKey.getQ().getRawXCoord().toBigInteger());
 		assertEquals(actualPublicKey.getQ().getRawYCoord().toBigInteger(),
-				expectedPublicKey.getQ().getRawYCoord().toBigInteger());
+			expectedPublicKey.getQ().getRawYCoord().toBigInteger());
 
 	}
-    
-  @Test
+
+	@Test
 	public void testSigning1() {
 		var keyPair = KeyManager.generateKeyPair();
 		var privateKey = (ECPrivateKeyParameters) keyPair.getPrivate();
@@ -72,25 +60,7 @@ class KeyManagerTest {
 	}
 
 	@Test
-	public void testEcParameters() {
-		// an example of the P-256 public private key pair.
-		var expectedPrivateKey = new BigInteger("6548d29d5533079e28d5c0edecb6394ed4c0342bfa475fdc96c7bf978492f23d", 16);
-		var expectedPublicKeyX = new BigInteger("fac843c2ccf0246eb00cb90e62dd96c64b669e2b867c0c2aaa835801aeae8295", 16);
-		var expectedPublicKeyY = new BigInteger("a797eb07a5d8eef80d5705a7a627c73cf4e683ab98c7245101025be935c4053b", 16);
-
-		var fpMultiplier = new FixedPointCombMultiplier();
-		ECPoint q = fpMultiplier.multiply(KeyManager.domainParams.getG(), expectedPrivateKey);
-
-		var actualPublicKey = new ECPublicKeyParameters(q, KeyManager.domainParams);
-		var actualPrivateKey = new ECPrivateKeyParameters(expectedPrivateKey, KeyManager.domainParams);
-
-		assertEquals(actualPrivateKey.getD(), expectedPrivateKey);
-		assertEquals(actualPublicKey.getQ().getRawXCoord().toBigInteger(), expectedPublicKeyX);
-		assertEquals(actualPublicKey.getQ().getRawYCoord().toBigInteger(), expectedPublicKeyY);
-
-	}
-  
-  public void testSigning2() {
+	public void testSigning2() {
 		var signature = Hex.decode("3045022100ff2c146535e75f0b5c8fb3548077f799429f72b4eb2e5412e89678b29fb165e3022036332b243d0f28fd05e9dca1be6eb1ff577622148e245732951fdfb71a4e684b");
 		var fakeSignature = Hex.decode("1045022100ff2c146535e75f0b5c8fb3548077f799429f72b4eb2e5412e89678b29fb165e3022036332b243d0f28fd05e9dca1be6eb1ff577622148e245732951fdfb71a4e684b");
 
