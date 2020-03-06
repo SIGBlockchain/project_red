211772265c8c5dacd639387ffb33a18276adf03a
jldistefano <43453042+jldistefano@users.noreply.github.com>
2020-03-05 19:07:16 -0600

    Fixed formatting for CheckSum.java

--- client/src/main/java/sigblockchain/projectred/client/AurumClient.java
+++ client/src/main/java/sigblockchain/projectred/client/AurumClient.java
@@ -1,11 +1,11 @@
 package sigblockchain.projectred.client;
 
 
+import com.google.gson.Gson;
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.URI;
-import com.google.gson.Gson;
 
 public class AurumClient {
 
